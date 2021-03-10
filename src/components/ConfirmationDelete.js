3034ccc550caea3c336b7d5a98b80479fe0ccab1
EryetChen <eryet201314kanna@gmail.com>
2021-03-10 07:53:45 +0000

    general ui update(got bug)

---
+++ src/components/ConfirmationDelete.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const ConfirmDelete = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Open alert dialog
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Delete?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {`do you confirm to delete x?`}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default ConfirmDelete
--- src/views/user/sensor/Sensor.js
+++ src/views/user/sensor/Sensor.js
@@ -1,27 +1,126 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 // component
-import AddToGroup from "./AddToGroup";
+import AddToGroup from './AddToGroup';
 
-const fakeSensorAPI = [
-  {
-    Name: "Sensor1",
-    Status: "Active",
-  },
-];
+// api
+import GatewayDetailAPI from '../../../fakeapi/GatewayDetailAPI';
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		display: 'block'
+	},
+	gateway_card: {
+		margin: 30
+	},
+	header: {
+		fontSize: '3em'
+	},
+	table: {
+		minWidth: 650
+	},
+	listtext: {
+		flex: 0.3
+	},
+	listtextlabel: {
+		flex: 0.3,
+		fontWeight: 'bold'
+	},
+	listitem: {
+		paddingTop: '20px',
+		paddingBtm: '20px'
+	}
+}));
 
 // sensor table with all sensor name, hyperlinked
 const Sensor = () => {
-  let { id } = useParams();
+	let { id } = useParams();
+	const classes = useStyles();
 
-  return (
-    <>
-      <h1>This page is Sensor Page</h1>
-      <AddToGroup />
-      <h1>{`render sensor number ${id}`}</h1>
-    </>
-  );
+	return (
+		<>
+			<h1>This page is Sensor Page</h1>
+			<h1>Sensor Detail</h1>
+			<Card className={classes.root}>
+				<CardContent>
+					<Paper elevation={0}>
+						<Box display={{ xs: 'none', sm: 'none', md: 'block', lg: 'block' }}>
+							<List component="nav" aria-label="profile">
+								<ListItem
+									button
+									divider
+									alignItems="flex-start"
+									className={classes.listitem}
+								>
+									<ListItemText
+										className={classes.listtext}
+										secondary="USERNAME"
+									/>
+									<ListItemText className={classes.listtext} primary={'text'} />
+
+									<ListItemSecondaryAction>
+										<NavigateNextIcon />
+									</ListItemSecondaryAction>
+								</ListItem>
+								<ListItem
+									button
+									divider
+									alignItems="flex-start"
+									className={classes.listitem}
+								>
+									<ListItemText
+										className={classes.listtext}
+										secondary="ROLES"
+									/>
+									<ListItemText className={classes.listtext} primary={'test'} />
+								</ListItem>
+							</List>
+						</Box>
+						<Box display={{ xs: 'block', sm: 'block', md: 'none', lg: 'none' }}>
+							<List component="nav" aria-label="profile">
+								<ListItem
+									button
+									divider
+									alignItems="flex-start"
+									className={classes.listitem}
+								>
+									<ListItemText
+										className={classes.listtext}
+										primary="USERNAME"
+										secondary={'test'}
+									/>
+									<ListItemSecondaryAction>
+										<NavigateNextIcon />
+									</ListItemSecondaryAction>
+								</ListItem>
+							</List>
+						</Box>
+					</Paper>
+				</CardContent>
+			</Card>
+			<AddToGroup />
+			<h1>{`render sensor number ${id}`}</h1>
+		</>
+	);
 };
 
 export default Sensor;
